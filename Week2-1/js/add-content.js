15401b26bb0ff19454b6baaa518851672e921c41
mathar <muhammad.athar@students.stamford.edu>
2015-11-26 22:58:50 +0700

    Week 2 Assignment 1 Step 1

--- Week2-1/js/add-content.js
+++ Week2-1/js/add-content.js
@@ -1,2 +1,12 @@
 // JavaScript Document
 
+var price;
+var quantity;
+var total;
+
+price = 5;
+quantity = 14;
+total = price * quantity;
+
+var totalPriceEle = document.getElementById("totalPrice");
+totalPriceEle.textContent = "$" + total;
