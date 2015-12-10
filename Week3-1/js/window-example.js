506418bdc8aa091e5f7279c5e59b2da78a897cc7
Muhammad <muhammad.athar@students.stamford.edu>
2015-12-09 17:22:04 -0800

    Upload Files and Finish Week 3 Assignment 1

---
+++ Week3-1/js/window-example.js
@@ -0,0 +1,27 @@
+var myWindow = {
+	screenx: window.screenX,
+	screeny: window.screenY,
+	screenHeight: window.screen.height,
+	screenWidth: window.screen.width,
+	locationn: window.location.pathname,
+
+	render: function() {
+		var ele = document.getElementById("location");
+		ele.value = this.locationn;
+
+		var eleX = document.getElementById("sx");
+		eleX.value = this.screenx;
+
+		var eleY = document.getElementById("sy");
+		eleY.value = this.screeny;
+
+		var eleW = document.getElementById("sw");
+		eleW.value = this.screenWidth;
+
+		var eleH = document.getElementById("sh");
+		eleH.value = this.screenHeight;
+	}
+};
+
+
+myWindow.render();
