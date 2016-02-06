cbe6a8740ad61ea6f5fd38b96a725a6e9f9f72a6
Muhammad <muhammad.athar@students.stamford.edu>
2016-02-06 16:20:46 +0700

    done in class

--- Week10/action.php
+++ Week10/action.php
@@ -1,21 +1,26 @@
 <?php 
-    requite_once("connection.php");
+	require_once("connection.php");
 	session_start();
 	$form = $_GET['q'];
 	if($form == "login") {
 		$username = $_POST['login-username'];
 		$password = $_POST['login-password'];
-		if($username == "admin" && $password == "1234") {
-			// Successfully login	
+        echo "username! ----" . $uusername. "<br>";
+        echo "password! ----".  $upassword. "<br>";
+        print_r $_POST;
+		//if(checkLogin($username, $password)){
+        //checkLogin($username, $password);
+		/*if($username == "admin" && $password == "1234"){
+			// Successfully login
 			$_SESSION['username'] = $username;
 			header("location:welcome.php");
 		}else {
 			header("location:index.php?login=error");
-		}
+		}*/
 		
-	}
-    else if ($form == "logout") {
+	}else if($form == "logout") {
 	    session_destroy();
+
 	    header("location:index.php");
-    }
+	}
 ?>
--- Week10/connection.php
+++ Week10/connection.php
@@ -1,24 +1,35 @@
 <?php
+	$hostname = "localhost:3306";
+	$username = "root";
+	$password = "Koolkat1";
+	$dbname = "ite220_userinfo";
 
+	$conn = new mysqli($hostname, $username, $password, $dbname);
+	if ($conn->connect_error) {
+		die($conn->connect_error);
+	}
+	/*else {
+		echo "connected";
+	}*/
+	function checklogin($uusername, $upassword){
+		global $conn;
+		$query = "SELECT * FROM users";
+		$result = $conn->query($query);
+		if (!$result) {
+			die($conn->error);
+		}
+		while ($row=mysqli_fetch_array($result)) {
+			echo "username". " ". $row["username"]. "<br>";
+			echo "password". " ". $row["password"]. "<br>";
+            echo "username ----" . $uusername. "<br>";
+            echo "password ----".  $upassword. "<br>";
+			if ($uusername == $row["username"] && md5($upassword) == $row["password"]) {
+				//return true;
+			}
 
-$hostname = "localhost:3306";
-$username = "root";
-$password = "";
-$dbname = "ite220_userinfo database";
+		}
+		//return false;
+	}
 
-$conn = new mysqli($hostname, $username, $password, $dbname);
-if ($conn -> connect_error) {
-    die ($conn -> connect_error);
-}
-
-
-function checklogin ($uusername, $upassword) {
-    $global $conn;
-    $query = "SELECT * FROM users";
-    $result = $conn -> query ($query);
-    if (!$result) {
-        die ($conn -> error);
-    }
-}
 
 ?>
